49e89e77097fd427123c5742305404084cdc0a2a
ak-online <79262567597@yandex.com>
2024-10-07 14:47:55 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,14 @@
+def apply_all_func(int_list, *functions):
+    results = dict()
+    #print(int_list)
+    #print(functions)
+    for func in functions:
+        results.update({func.__name__: func(int_list)})
+    return results,
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
+
